57aa5263f500a12b75b1fbe42608059b3db28835
Alexander <scorgi86@gmail.com>
2014-04-09 13:18:19 +0600

    qwe

---
+++ themes/koreyana/views/page/contacts.php
@@ -0,0 +1,39 @@
+<form id="form-services" action="#">
+    <div class="row">
+        <div class="col-lg-4">
+            <label for="FirstName">Ваше имя</label>
+        </div>
+        <div class="col-lg-8">
+            <input type="text" name="FirstName" placeholder="Ваше имя" value=""> 
+        </div>
+    </div>                 
+    <div class="row">
+        <div class="col-lg-4">
+            <label for="Tel">Телефон</label>
+        </div>
+        <div class="col-lg-8">
+            <input class="phone_us" type="text" name="Tel" placeholder="+7 (___) ___-__-__" value="">
+        </div>
+    </div>                     
+    <div class="row">
+        <div class="col-lg-4">
+            <label for="mail">E-mail</label> 
+        </div>
+        <div class="col-lg-8">
+            <input type="text" name="mail" placeholder="example@example.com" value="">
+        </div>
+    </div>                                   
+    <div class="row">
+        <div class="col-lg-4">
+            <label>Текст обращения</label>
+        </div>
+        <div class="col-lg-8">
+            <textarea rows="11" cols="55" placeholder="Текст сообщения..."></textarea>
+        </div>
+    </div>    
+    <div class="row">
+        <div class="bt-4"> 
+            <a href="#">Отправить</a>
+        </div>
+    </div>
+</form>
