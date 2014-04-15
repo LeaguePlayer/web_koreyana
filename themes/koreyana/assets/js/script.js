9237dca5b385d17c2d99674ba1604565837bc722
mmmsharap <mmmsharap@gmail.com>
2014-04-15 15:23:15 +0500

    add map placemark

--- themes/koreyana/assets/js/script.js
+++ themes/koreyana/assets/js/script.js
@@ -94,4 +94,33 @@ $(document).ready(function(){
     $('.date').mask('99/99/9999');
     $('.time').mask('99:99');
     $('.Tel').mask('+7 (000) 000-00-00');
+
+    //Яндекс карта
+    YMaps.jQuery(function () {
+    		if ($('#map-1').length) {
+	            map = new YMaps.Map(document.getElementById("map-1"));
+	            map.setCenter(new YMaps.GeoPoint(65.610002,57.152867), 17);
+	            map.addControl(new YMaps.TypeControl());
+				map.addControl(new YMaps.ToolBar());
+				map.addControl(new YMaps.Zoom());
+	            var placemark = new YMaps.Placemark(new YMaps.GeoPoint(65.610002,57.152867), {style: "default#carIcon"});
+	            placemark.name = "Центральный филиал: ул. Дамбовская, 10";
+				placemark.description = "+7 (3452) 500-480 (отдел продаж)<br /> +7 (3452) 986-185 (СТО)<br /> +7 (3452) 637-909<br /> +7 (3452) 710-999";
+
+	            map.addOverlay(placemark);
+    		}
+    		if ($('#mymap').length) {
+	            map = new YMaps.Map(document.getElementById("mymap"));
+	            map.setCenter(new YMaps.GeoPoint(65.610002,57.152867), 17);
+	            map.addControl(new YMaps.TypeControl());
+				map.addControl(new YMaps.ToolBar());
+				map.addControl(new YMaps.Zoom());
+	            var placemark = new YMaps.Placemark(new YMaps.GeoPoint(65.610002,57.152867), {style: "default#carIcon"});
+	            placemark.name = "Центральный филиал: ул. Дамбовская, 10";
+				placemark.description = "+7 (3452) 500-480 (отдел продаж)<br /> +7 (3452) 986-185 (СТО)<br /> +7 (3452) 637-909<br /> +7 (3452) 710-999";
+
+	            map.addOverlay(placemark);
+    		}
+        });
+
 });
--- themes/koreyana/views/page/view.php
+++ themes/koreyana/views/page/view.php
@@ -2,7 +2,7 @@
 $cs = Yii::app()->clientScript;
 $pathToAssets=$this->getAssetsUrl();
 
-$cs->registerScriptFile('http://api-maps.yandex.ru/services/constructor/1.0/js/?sid=PC5YaAxHJF303X_pR-LSyeodnO-oicuY&id=mymap', CClientScript::POS_END);
+//$cs->registerScriptFile('http://api-maps.yandex.ru/services/constructor/1.0/js/?sid=PC5YaAxHJF303X_pR-LSyeodnO-oicuY&id=mymap', CClientScript::POS_END);
 //$cs->registerScriptFile($pathToAssets.'/js/'.'jquery.mask.js', CClientScript::POS_END);
 
 print($model->wswg_body);
