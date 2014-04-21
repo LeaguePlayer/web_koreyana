928aa3dbe309205619ea830bd95796d2dd0e7298
Alexander <scorgi86@gmail.com>
2014-04-21 17:08:29 +0600

    qwe

--- protected/modules/admin/views/record/update.php
+++ protected/modules/admin/views/record/update.php
@@ -1,6 +1,6 @@
 <?php
 $this->breadcrumbs=array(
-	"{$model->translition()}"=>array('list'),
+	"Записи"=>array('list'),
 	'Редактирование',
 );
 
@@ -10,6 +10,6 @@ $this->menu=array(
 );
 ?>
 
-<h1><?php echo $model->translition(); ?> - Редактирование</h1>
+<h1><?php echo "Записи"; ?> - Редактирование</h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
